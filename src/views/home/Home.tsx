0cd30868b1cb41c89c7ebbf4dda224a9a529f45f
Ariel Arias <arielwwjd@gmail.com>
2023-06-09 16:04:32 -0400

    pages styles and functionality updated to hold the courseCards

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,8 @@
 import { useNavigate } from 'react-router-dom';
-import { currentPageHeaderItem, headerItems } from '../../utils';
+import { CourseCardList } from '../../components/CourseCardList';
+import { currentPageHeaderItem, headerItems, cards } from '../../utils';
 import { Layout } from '../layout';
-import { CourseCard } from '../../components/courseCard';
+import './styles.scss';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -11,31 +12,19 @@ const Home = () => {
   };
 
   const headerItemsLayout = headerItems(navigate);
-  const description =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...';
 
   return (
-    <Layout
-      headerItems={headerItemsLayout}
-      onRegisterOrLoginOrLogout={onRegisterOrLoginHandler}
-      currentPage={currentPageHeaderItem.home}
-      registerOrLoginOrLogoutLabel='Register or Login'
-    >
-      <CourseCard
-        id='java'
-        name='Java'
-        title='Java Advanced'
-        description={description}
-        duration='3:20hr'
-        review={{ stars: 4.5, people: 422 }}
-        action={{
-          buttonLabel: 'Enroll',
-          onButtonClick: () => {
-            console.log('click');
-          },
-        }}
-      />
-    </Layout>
+    <div className='home-view'>
+      <Layout
+        headerItems={headerItemsLayout}
+        onRegisterOrLoginOrLogout={onRegisterOrLoginHandler}
+        currentPage={currentPageHeaderItem.home}
+        registerOrLoginOrLogoutLabel='Register or Login'
+      >
+        <div className='title'>Available Courses</div>
+        <CourseCardList cards={cards} />
+      </Layout>
+    </div>
   );
 };
 
--- src/views/myCourses/MyCourses.tsx
+++ src/views/myCourses/MyCourses.tsx
@@ -1,25 +1,32 @@
 import { useNavigate } from 'react-router-dom';
-import { currentPageHeaderItem, headerItems } from '../../utils';
+import { currentPageHeaderItem, headerItems, myCards } from '../../utils';
+import { CourseCardList } from '../../components/CourseCardList';
 import { Layout } from '../layout';
+import './styles.scss';
 
 const MyCourses = () => {
   const navigate = useNavigate();
 
   const onRegisterOrLoginHandler = () => {
-    console.log('REGISTER OR LOGIN');
+    console.log('LOGOUT');
   };
 
   const headerItemsLayout = headerItems(navigate);
 
   return (
-    <Layout
-      headerItems={headerItemsLayout}
-      onRegisterOrLoginOrLogout={onRegisterOrLoginHandler}
-      currentPage={currentPageHeaderItem.myCourses}
-      registerOrLoginOrLogoutLabel='Logout'
-    >
-      My Courses Page
-    </Layout>
+    <div className='my-courses-view'>
+      <Layout
+        headerItems={headerItemsLayout}
+        onRegisterOrLoginOrLogout={onRegisterOrLoginHandler}
+        currentPage={currentPageHeaderItem.myCourses}
+        registerOrLoginOrLogoutLabel='Logout'
+      >
+        <div className='title'>My Courses</div>
+        <div className='cards-container'>
+          <CourseCardList cards={myCards} isLogged />
+        </div>
+      </Layout>
+    </div>
   );
 };
 
