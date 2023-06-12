0ed909703932d420e49a4f7f129cce36613fe827
Ariel Arias <arielwwjd@gmail.com>
2023-06-12 18:39:46 -0400

    home view updated to hold the modal component

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CourseCardList } from '../../components/CourseCardList';
 import { currentPageHeaderItem, headerItems, cards } from '../../utils';
+import { Modal } from '../../components/modal';
 import { Layout } from '../layout';
 import './styles.scss';
+import { ModalContent } from '../../components/modalContent';
 
 const Home = () => {
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState<boolean>(true);
 
   const onRegisterOrLoginHandler = () => {
     console.log('REGISTER OR LOGIN');
+  };
+
+  const closeModalHandler = () => {
+    setShowModal(false);
   };
 
   const headerItemsLayout = headerItems(navigate);
 
   return (
     <div className='home-view'>
+      <Modal isVisible={showModal}>
+        <ModalContent
+          onClose={closeModalHandler}
+          type='loginOrSignUp'
+        ></ModalContent>
+      </Modal>
       <Layout
         headerItems={headerItemsLayout}
         onRegisterOrLoginOrLogout={onRegisterOrLoginHandler}
