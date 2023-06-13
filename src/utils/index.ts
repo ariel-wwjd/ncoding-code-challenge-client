96576fcfd0ea3da17dd49d08dc3c351fb0c955a9
Ariel Arias <arielwwjd@gmail.com>
2023-06-13 13:53:42 -0400

    ultis data added to hold the payment plans data

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { classNames, className } from './classNames';
 export { headerItems, currentPageHeaderItem } from './navMenuItems';
 export { cards, myCards } from './cards';
+export { paymentPlans } from './paymentPlans';
---
+++ src/utils/paymentPlans.ts
@@ -0,0 +1,47 @@
+export const paymentPlans = (onClick: (id: string) => void) => {
+  return [
+    {
+      id: 'plan1',
+      name: 'PIF (pay in Full)',
+      amount: '$6.795',
+      totalAmount: '$4.795',
+      save: 'Save $1500 in tuition',
+      note: 'No processing fees',
+      onClick: () => {
+        onClick('plan1');
+      },
+    },
+    {
+      id: 'plan2',
+      name: '16 installments ',
+      amount: 'of $399 / Month',
+      save: 'Save $500 in tuition',
+      note: 'No processing fees',
+      interest: '0% interest rate',
+      onClick: () => {
+        onClick('plan2');
+      },
+    },
+    {
+      id: 'plan3',
+      name: '24 installments',
+      amount: 'of $299 / Month',
+      save: 'Save $250 in tuition',
+      note: 'No processing fees',
+      interest: '5.99% interest rate',
+      onClick: () => {
+        onClick('plan3');
+      },
+    },
+    {
+      id: 'plan4',
+      name: '48 installments',
+      amount: 'of $199 / Month',
+      note: 'No processing fees',
+      interest: '11.9% interest rate',
+      onClick: () => {
+        onClick('plan4');
+      },
+    },
+  ];
+};
